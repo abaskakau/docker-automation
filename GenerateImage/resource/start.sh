90c55a524732a9e2cc2b80516c30d5315e8a79d5
Andrei Baskakau <Andrei_Baskakau@epam.com>
2016-08-16 16:39:05 +0300

    Initial#1

--- GenerateImage/resource/start.sh
+++ GenerateImage/resource/start.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-sh .installer.sh
-
 bash $PENTAHO_HOME/ctlscript.sh start postgresql
 bash $PENTAHO_HOME/ctlscript.sh start baserver
 bash $PENTAHO_HOME/ctlscript.sh start data-integration-server
